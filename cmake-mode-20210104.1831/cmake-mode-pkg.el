cdc834ee9acccbc1750b36e2f74d64fd267df9cd
Loris Lucido <loris.lucido@atos.net>
2022-06-02 10:37:21 +0200

    init emacs

---
+++ cmake-mode-20210104.1831/cmake-mode-pkg.el
@@ -0,0 +1,2 @@
+;;; Generated package description from cmake-mode.el  -*- no-byte-compile: t -*-
+(define-package "cmake-mode" "20210104.1831" "major-mode for editing CMake sources" '((emacs "24.1")) :commit "0af3c2e78c50c9552d2173dd3a32b825b71dd7df")
