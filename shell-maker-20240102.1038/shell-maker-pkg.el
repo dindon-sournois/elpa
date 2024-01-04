27d66fb67ad516fb28ed6ba600fc21d5478c743c
Loris Lucido <loris.lucido@eviden.com>
2024-01-04 11:27:01 +0100

    up packages

---
+++ shell-maker-20240102.1038/shell-maker-pkg.el
@@ -0,0 +1,12 @@
+(define-package "shell-maker" "20240102.1038" "Interaction mode for making comint shells"
+  '((emacs "27.1"))
+  :commit "0a25e185a89044ee0391def5c58b9d8f4a2e9a56" :authors
+  '(("Alvaro Ramirez https://xenodium.com"))
+  :maintainers
+  '(("Alvaro Ramirez https://xenodium.com"))
+  :maintainer
+  '("Alvaro Ramirez https://xenodium.com")
+  :url "https://github.com/xenodium/chatgpt-shell")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
