c018458a67065fd589fc1ab84b6767ca6cce5988
Loris Lucido <loris.lucido@eviden.com>
2024-11-14 14:07:05 +0100

    packages upgrade

---
+++ chatgpt-shell-20241113.2229/chatgpt-shell-pkg.el
@@ -0,0 +1,8 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "chatgpt-shell" "20241113.2229"
+  "ChatGPT shell + buffer insert commands."
+  '((emacs       "28.1")
+    (shell-maker "0.62.1"))
+  :url "https://github.com/xenodium/chatgpt-shell"
+  :commit "fa383b47ed258c1d7b61d7399df02f74a0f8aa98"
+  :revdesc "fa383b47ed25")
---
+++ shell-maker-20241112.1906/shell-maker-pkg.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "shell-maker" "20241112.1906"
+  "Interaction mode for making comint shells."
+  '((emacs "27.1"))
+  :url "https://github.com/xenodium/shell-maker"
+  :commit "a0452a1c832a7ca6ecfa3e5a34d695a25b2a22a4"
+  :revdesc "a0452a1c832a")
