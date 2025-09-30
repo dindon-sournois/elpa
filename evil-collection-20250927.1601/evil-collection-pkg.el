bcd4b8c173cdcb8e46a52c70a41cf8a8b0843bbd
dindon-sournois <dindon@zaclys.net>
2025-09-30 17:28:39 +0200

    upgrade elpa packages

---
+++ evil-collection-20250927.1601/evil-collection-pkg.el
@@ -0,0 +1,12 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "evil-collection" "20250927.1601"
+  "A set of keybindings for Evil mode."
+  '((emacs    "26.3")
+    (evil     "1.2.13")
+    (annalist "1.0"))
+  :url "https://github.com/emacs-evil/evil-collection"
+  :commit "f59dacc9fd8501631cbefc09057c91ed95cdf318"
+  :revdesc "f59dacc9fd85"
+  :keywords '("evil" "tools")
+  :authors '(("James Nguyen" . "james@jojojames.com"))
+  :maintainers '(("James Nguyen" . "james@jojojames.com")))
---
+++ shell-maker-20250923.1433/shell-maker-pkg.el
@@ -0,0 +1,7 @@
+;; -*- no-byte-compile: t; lexical-binding: nil -*-
+(define-package "shell-maker" "20250923.1433"
+  "Interaction mode for making comint shells."
+  '((emacs "27.1"))
+  :url "https://github.com/xenodium/shell-maker"
+  :commit "547cb01c82060232e78a57db7d002da65ac8b773"
+  :revdesc "547cb01c8206")
