b6c9a74e5afdfa0b0676830b460bc9bfee371d49
Loris Lucido <loris.lucido@eviden.com>
2026-01-08 11:25:09 +0100

    gptel

---
+++ gptel-0.9.9/gptel-pkg.el
@@ -0,0 +1,2 @@
+;; Generated package description from gptel.el  -*- no-byte-compile: t -*-
+(define-package "gptel" "0.9.9" "Interact with ChatGPT or other LLMs" '((emacs "27.1") (transient "0.7.4") (compat "30.1.0.0")) :commit "81618f24e2190568ea745dca82ba50267eed321a" :authors '(("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com")) :maintainer '("Karthik Chikmagalur" . "karthik.chikmagalur@gmail.com") :keywords '("convenience" "tools") :url "https://github.com/karthink/gptel")
